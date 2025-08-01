959cadfe9c805835d443c3f80e51bbedfe76380d
Søren Krogh <soren.krogh@alexandra.dk>
2025-08-01 08:52:40 +0200

    Minor linting change

--- src/components/PWAInstallPrompt.tsx
+++ src/components/PWAInstallPrompt.tsx
@@ -96,6 +96,7 @@ export default function PWAInstallPrompt() {
       window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
       window.removeEventListener('appinstalled', handleAppInstalled);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleInstallClick = async () => {
